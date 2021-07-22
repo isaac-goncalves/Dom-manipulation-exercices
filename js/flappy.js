819c2effd3664970a6cb5023e7328f3581bf1cbc
isaac-goncalves <isaac.correia.2406@gmail.com>
2021-07-22 17:12:58 -0300

    Teste

---
+++ js/flappy.js
@@ -0,0 +1,52 @@
+function novoElemento(tagName, className) {
+    const elem = document.createElement(tagName)
+    elem.className = className
+    return elem
+}
+
+class Barreira {
+    constructor(reversa = false) {
+        this.elemento = novoElemento('div', 'barreira')
+
+        const borda = novoElemento('div', 'borda')
+        const corpo = novoElemento('div', 'corpo')
+
+        this.elemento.appendChild(reversa ? corpo : borda)
+        this.elemento.appendChild(reversa ? borda : corpo)
+
+        this.setAltura = altura => corpo.style.height = `${altura}px`
+
+    }
+}
+
+
+// const b = new Barreira(true)
+// b.setAltura(500)
+// document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+function PardeBarreiras(altura, abertura, x) {
+    this.elemento = novoElemento('div', 'par-de-barreiras')
+
+    this.superior = new Barreira(true)
+    this.inferior = new Barreira(false)
+
+    this.elemento.appendChild(this.superior.elemento)
+    this.elemento.appendChild(this.inferior.elemento)
+
+    this.sortearAbertura = () => {
+        const aberturaSuperior = Math.random() * (altura - abertura)
+        const aberturaInferior = altura - abertura - aberturaSuperior
+        this.superior.setAltura(aberturaSuperior)
+        this.inferior.setAltura(aberturaInferior)
+    }
+
+    this.getX = () => parseInt(this.elemento.style.left.split('px')[0])
+    this.setX = x => this.elemento.style.left = `${x}px`
+    this.getLargura = () => history.elemento.clientWidth
+
+    this.sortearAbertura()
+    this.setX(x)
+}
+const b = new PardeBarreiras(700, 200, 400)
+document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
