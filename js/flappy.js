6d05c0e665779b5aa59f84ae73fdd88af6912b2b
isaac-goncalves <isaac.correia.2406@gmail.com>
2021-07-23 11:26:23 -0300

    Flappy changes

--- js/flappy.js
+++ js/flappy.js
@@ -47,6 +47,27 @@ function PardeBarreiras(altura, abertura, x) {
     this.sortearAbertura()
     this.setX(x)
 }
-const b = new PardeBarreiras(700, 200, 400)
-document.querySelector('[wm-flappy]').appendChild(b.elemento)
+// const b = new PardeBarreiras(700, 200, 400)
+// document.querySelector('[wm-flappy]').appendChild(b.elemento)
+
+function Barreiras(altura, largura, abertura, espaco, notificarPonto) {
+    this.pares = [
+        new PardeBarreiras(altura, abertura, largura),
+        new PardeBarreiras(altura, abertura, largura + espaco),
+        new PardeBarreiras(altura, abertura, largura + espaco * 2),
+        new PardeBarreiras(altura, abertura, largura + espaco * 3)
+    ]
+    const deslocamento = 3
+
+    this.animar = () => {
+        this.pares.forEach(par => {
+            par.setX(par.getX() - deslocamento)
+
+            if(par.getX() < -par.getLargura()) {
+                par.setX(par.getX() + espaco *this.pares.length)
+            }
+        }
+        )
+    }
+}
 
